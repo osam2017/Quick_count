a684c689856bb110c715b90a3931fb30d9a8fea0
pjhu123 <pjhu@naver.com>
2017-10-20 00:58:55 +0900

    액티비티 추가생성

---
+++ app/src/main/java/com/example/administrator/quickcount/Count.java
@@ -0,0 +1,53 @@
+package com.example.administrator.quickcount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Count extends AppCompatActivity {
+
+    EditText inBoxEditText, boxXEditText, boxYEditText, boxZEditText, emptyEditText, liteEditText;
+    Button countButton;
+    String int1,int2, int3, int4, int5, int6;
+    Integer result;
+    TextView textResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count);
+
+        inBoxEditText =(EditText)findViewById(R.id.inBoxEditText);
+        boxXEditText =(EditText)findViewById(R.id.boxXEditText);
+        boxYEditText =(EditText)findViewById(R.id.boxYEditText);
+        boxZEditText =(EditText)findViewById(R.id.boxZEditText);
+        emptyEditText =(EditText)findViewById(R.id.emptyEditText);
+        liteEditText =(EditText)findViewById(R.id.liteEditText);
+
+
+
+
+        countButton= (Button)findViewById(R.id.countButton);
+
+        textResult=(TextView)findViewById(R.id.resultTextView);
+
+
+        countButton.setOnClickListener (new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int1 = inBoxEditText.getText().toString();
+                int2 = boxXEditText.getText().toString();
+                int3 = boxYEditText.getText().toString();
+                int4 = boxZEditText.getText().toString();
+                int5 = emptyEditText.getText().toString();
+                int6 = liteEditText.getText().toString();
+                result = Integer.parseInt(int1) * (Integer.parseInt(int2)*Integer.parseInt(int3)*Integer.parseInt(int4)-Integer.parseInt(int5)-1)+Integer.parseInt(int6);
+                textResult.setText(result.toString()+" R");
+            }
+        });
+
+    }
+}
