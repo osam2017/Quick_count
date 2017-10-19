40ca6dc76b6bd26c0391fe86ba61bddf1326f5bc
pjhu123 <pjhu@naver.com>
2017-10-20 01:21:54 +0900

    액티비티간 연결

--- app/src/main/java/com/example/administrator/quickcount/MainActivity.java
+++ app/src/main/java/com/example/administrator/quickcount/MainActivity.java
@@ -1,17 +1,44 @@
 package com.example.administrator.quickcount;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 
 
-public class MainActivity extends AppCompatActivity  {
-
+public class MainActivity extends AppCompatActivity  implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button count = (Button) findViewById(R.id.count);
+        Button tap = (Button) findViewById(R.id.tap);
+        count.setOnClickListener(this);
+        tap.setOnClickListener(this);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intentCount = new Intent(this, Count.class);
+        Intent intentTap = new Intent(this, Tap.class);
+        switch (v.getId()) {
+            case R.id.count:
+                startActivity(intentCount);
+                break;
+
+            case R.id.tap:
+                startActivity(intentTap);
+                break;
+
+
+        }
 
     }
 }
---
+++ app/src/main/java/com/example/administrator/quickcount/Tap.java
@@ -0,0 +1,13 @@
+package com.example.administrator.quickcount;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tap);
+    }
+}
